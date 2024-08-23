95667a33ad90883bb5ae8337f6a7eea91e07fc9d
time0ut <84389383+time0ut07@users.noreply.github.com>
2024-08-23 15:23:40 +0800

    Create install_portainer.sh

---
+++ scripts/install_portainer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function error_message {
+    echo -e "//e[91m\n$1//e[39m"
+    exit 1
+}
+
+function internet_status() {
+    echo  "[*] Determining internet status..."
+    wget -q --spider http://github.com
+    if [ $? -eq 0 ]; then
+        echo "[+] You are online!"
+    else
+        error_message "[-] You are offline... go connect to the internet first!"
+    fi
+}
+
+function install_portainer() {
+    sudo docker pull portainer/portainer-ce:latest || error "[-] Failed to pull Portainer docker image!"
+    sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest || erro>
+}
+
+echo -e "time0ut - install_portainer.sh"
+echo
+internet_status
+echo
+install_portainer

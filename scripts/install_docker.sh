bf87e3b31144b720a8271362dce6f1c18de71b70
time0ut <84389383+time0ut07@users.noreply.github.com>
2024-08-24 12:04:17 +0100

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -16,8 +16,15 @@ function internet_status() {
 }
 
 function install_docker() {
+    echo "[*] Installing docker..."
     curl -sSL https://get.docker.com | sh || error_message "[-] Failed to install docker..."
     sudo usermod -aG docker $USER || error_message "[-] Failed to add user into Docker user group..."
+}
+
+function install_dockercompose() {
+    echo "[*] Installing docker compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose | sh || error_message "[-] Failed to install docker compose..."
+    sudo chmod +x /usr/local/bin/docker-compose | sh || error_message "[-] Failed to +x /usr/local/bin/docker-compose..."
 }
 
 echo -e "time0ut - install_docker.sh"
@@ -25,4 +32,7 @@ echo
 internet_status
 echo
 install_docker
+echo
+install_dockercompose
+echo
 echo "[+] Reboot machine for the changes to take effect!"

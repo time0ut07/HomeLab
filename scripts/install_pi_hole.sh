225d88d4d5a01bf60ad11f1258a06083641adf82
time0ut <84389383+time0ut07@users.noreply.github.com>
2024-09-04 05:39:37 +0100

    Create install_pi_hole.sh

---
+++ scripts/install_pi_hole.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+function error_message {
+    echo -e "//e[91m\n$1//e[39m"
+    exit 1
+}
+
+function internet_status() {
+    echo  "[*] Determining internet status..."
+    wget -q --spider http://github.com
+    if [ $? -eq 0 ]; then
+        echo "[+] You are online!"
+    else
+        error_message "[-] You are offline... go connect to the internet first!"
+    fi
+}
+
+function create_pi_hole() {
+  echo "[*] Creating directories..."
+  sudo mkdir -p /home/$USER/docker/pi_hole || error_message "[x] Failed to create pihole folder!"
+
+  echo "[*] Downloading filebrowser container..."
+  docker pull pihole/pihole:latest
+
+  echo "[*] Initialising filebrowser..."
+  docker run -d \
+      --name pihole \
+      --restart=unless-stopped \
+      -e TZ="Asia/Kolkata" \
+      -v "/home/$USER/pi_hole/etc-pihole:/etc/pihole" \
+      -v "/home/$USER/pi_hole/etc-dnsmasq.d:/etc/dnsmasq.d" \
+      -e VIRTUAL_HOST="pi.hole" \
+      -e PROXY_LOCATION="pi.hole" \
+      -e FTLCONF_LOCAL_IPV4="127.0.0.1" \ # might need to change to external ip
+      --dns=127.0.0.1 --dns=1.1.1.1 \
+      --hostname pi.hole \
+      -p 53:53/tcp -p 53:53/udp \
+      -p 8002:80 \
+      pihole/pihole:latest
+}
+
+echo -e "time0ut - install pi_hole.sh"
+internet_status
+create_pi_hole
+echo "[+] Successfully created a pihole docker container!"
+echo
+echo "Visit me @ http://localhost:8002"

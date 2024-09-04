697cd3b8f0db1f256bd0d472747134e10300f675
time0ut <84389383+time0ut07@users.noreply.github.com>
2024-09-04 06:02:00 +0100

    Update install_pi_hole.sh

--- scripts/install_pi_hole.sh
+++ scripts/install_pi_hole.sh
@@ -19,9 +19,6 @@ function create_pi_hole() {
   echo "[*] Creating directories..."
   sudo mkdir -p /home/$USER/docker/pi_hole || error_message "[x] Failed to create pihole folder!"
 
-  echo "[*] Downloading pihole container..."
-  docker pull pihole/pihole:latest
-
   echo "[*] Initialising pihole..."
   docker run -d \
       --name pihole \
@@ -31,7 +28,7 @@ function create_pi_hole() {
       -v "/home/$USER/docker/pi_hole/etc-dnsmasq.d:/etc/dnsmasq.d" \
       -e VIRTUAL_HOST="pi.hole" \
       -e PROXY_LOCATION="pi.hole" \
-      -e FTLCONF_LOCAL_IPV4="127.0.0.1" \ # might need to change to external ip
+      -e FTLCONF_LOCAL_IPV4="127.0.0.1" \
       --dns=127.0.0.1 --dns=1.1.1.1 \
       --hostname pi.hole \
       -p 53:53/tcp -p 53:53/udp \
@@ -39,9 +36,29 @@ function create_pi_hole() {
       pihole/pihole:latest
 }
 
+function start_pi_hole() {
+    printf "[*] Starting up pihole container..."
+    for i in $(seq 1 20); do
+        if [ "$(docker inspect -f "{{.State.Health.Status}}" pihole)" == "healthy" ] ; then
+            printf ' OK'
+            echo -e "\n$(docker logs pihole 2> /dev/null | grep 'password:') for your pi-hole: http://${IP}/admin/"
+            exit 0
+        else
+            sleep 3
+            printf '.'
+        fi
+    
+        if [ $i -eq 20 ] ; then
+            echo -e "\nTimed out waiting for Pi-hole start, consult your container logs for more info (\`docker logs pihole\`)"
+            exit 1
+        fi
+}
+
 echo -e "time0ut - install pihole.sh"
 internet_status
 create_pi_hole
 echo "[+] Successfully created a pihole docker container!"
+start_pi_hole
+echo "[+] Successfully started pihole!"
 echo
 echo "Visit me @ http://localhost:8002"
